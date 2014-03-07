35ec9bff30c5fba587672d215b3d47f1139d6b44
Nikolaus Schlemm <capo@coder-nostra.com>
2014-03-07 13:48:54 +0100

    pep8 compliance

--- themeswitch/__init__.py
+++ themeswitch/__init__.py
@@ -1,3 +1,9 @@
+def _get_bootswatch_css_url(theme, bootstrap_version='3.0.0'):
+    "little helper to generate a bootswatch theme's css url"
+    css_url = '//netdna.bootstrapcdn.com/bootswatch/%s/%s/bootstrap.min.css'
+    return css_url % (bootstrap_version, theme)
+
+
 BOOTSWATCHES = (
     'amelia',
     'cerulean',
@@ -12,8 +18,9 @@ BOOTSWATCHES = (
     'united',
 )
 
+
 BOOTSWATCH_THEMES = dict(
-    (theme, '//netdna.bootstrapcdn.com/bootswatch/3.0.0/%s/bootstrap.min.css' % theme)
+    (theme, _get_bootswatch_css_url(theme))
     for theme in BOOTSWATCHES
 )
 
--- themeswitch/context_processors.py
+++ themeswitch/context_processors.py
@@ -2,8 +2,7 @@ from themeswitch import THEMES
 
 
 def selected_theme(request):
-    if 'selected_theme' in request.COOKIES and \
-            request.COOKIES['selected_theme'] in THEMES:
+    if request.COOKIES.get('selected_theme', None) in THEMES:
         return {'selected_theme': request.COOKIES['selected_theme']}
 
     return {'selected_theme': None}
