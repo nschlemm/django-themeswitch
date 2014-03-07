49851db8d205c4545c481807cdf7a5865056ae0c
Nikolaus Schlemm <capo@coder-nostra.com>
2014-03-07 13:49:53 +0100

    raise ImproperlyConfigured exception with hint about missing TEMPLATE_CONTEXT_PROCESSOR

--- themeswitch/templatetags/themeswitch_tags.py
+++ themeswitch/templatetags/themeswitch_tags.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.template.base import Library
 
 from themeswitch import THEMES
@@ -7,6 +9,12 @@ register = Library()
 
 @register.simple_tag(takes_context=True)
 def render_selected_theme_css(context):
+    if not 'selected_theme' in context:
+        context_processor = 'themeswitch.context_processors.selected_theme'
+        if context_processor not in settings.TEMPLATE_CONTEXT_PROCESSORS:
+            raise ImproperlyConfigured(
+                'add %s to TEMPLATE_CONTEXT_PROCESSORS' % context_processor
+            )
     selected_theme = context['selected_theme']
     if selected_theme and selected_theme in THEMES:
         return u'<link href="%s" rel="stylesheet">' % THEMES[selected_theme]
